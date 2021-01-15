c0a327da50029f1b1f02db4855e5252e04b1b007
Will Leonard <wleonard@salsify.com>
2021-01-15 15:58:28 -0500

    Update rubocop and fix errors

--- lib/active_record/forbid_implicit_connection_checkout/version.rb
+++ lib/active_record/forbid_implicit_connection_checkout/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module ActiveRecord
   module ForbidImplicitConnectionCheckout
-    VERSION = '2.0.0'.freeze
+    VERSION = '2.0.0'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'active_record'
 require 'active_record-forbid_implicit_connection_checkout'
 require 'fileutils'
@@ -17,7 +19,7 @@ db_config = YAML.safe_load(File.read('spec/db/database.yml'))
 db_host = db_config[db_adapter]['host']
 ENV['PGHOST'] ||= db_host if db_host
 
-DATABASE_NAME = 'forbid_implicit_connection_checkout_test'.freeze
+DATABASE_NAME = 'forbid_implicit_connection_checkout_test'
 
 RSpec.configure do |config|
   config.order = 'random'
@@ -31,15 +33,15 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do
+  config.before do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each) do
+  config.before do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.after do
     DatabaseCleaner.clean
   end
 end
